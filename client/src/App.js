d3fc1f524ad4289e92492abba8027476de4aa2f0
Sara Dehghani <saradma@gmail.com>
2019-11-18 17:03:10 -0500

    created router for front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render(){
+    return(
+      <div className="App">
+       <BrowserRouter>
+       {/* <Header/> */}
+        <Switch>
+         <Route path="/" exact component={Warehouses}></Route>
+         <Route path="/warehouses" component={Warehouses}></Route>
+         <Route path="/inventory" component={Inventory}></Route>
+         <Route path="/warehouses/:id" exact component={Warehousesdetails}></Route>
+         <Route path="/inventory/:id" exact component={Inventorydetails}></Route>
+        </Switch>
+       </BrowserRouter>
+      </div>
+    )
+  }
 }
-
 export default App;
