c0810ea75f2feb26bb618ac2c8e7c69f005a4675
Sara Dehghani <saradma@gmail.com>
2019-11-19 10:25:37 -0500

    added delete inventoryitem and rout

--- server/Routes/inventory.js
+++ server/Routes/inventory.js
@@ -11,4 +11,12 @@ router.get('/',(request,response) => {
     response.send(inventoryData);
 })
 
+router.delete('/:id',(request,response)=>{
+    if (request.params.id){
+      const inventoryList=inventoryData.filter(item=>item.id!==request.params.id)
+      response.send(inventoryList)
+      }else{
+       response.status(400)
+      }
+})
 module.exports = router;
