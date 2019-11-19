82e8b512c4fb543248acc498694d51771ed9c6ac
Redwan Zia <redwan.zia@gmail.com>
2019-11-19 15:28:11 -0500

    added post request endpoint for new added location

--- server/Routes/warehouse.js
+++ server/Routes/warehouse.js
@@ -31,13 +31,33 @@ router.get("/:id",(request,response)=>{
 
     if(findInv){
         response.send(findInv)
-    }else{ response.status(404).send("no inventory")}
-
-
-  
+    }else{ response.status(404).send("no inventory")} 
 
    
 })
+
+router.post ('/', (request,  response=>{
+    const newAddedLocation ={
+
+        "warehouse":request.body.warehouse,
+        "address":request.body.address,
+        "contact name":request.body.contact name,
+        "phone number":request.body.phone number,
+        "categories":request.body.categories,
+        "location":request.body.location,
+        "position":request.body.position,
+        "email":request.body.email,
+    }
+
+    if (request.body.warehouse && request.body.address && request.body.address && request.body.contactname && request.body.phonenumber && request.body.categories && request.body.location && request.body.position && request.body.email){
+        locationData.push(newAddedLocation)
+        response.send(locationData)
+    }
+    else{
+        return response.status(400).send('Data is Malformed or a Field is Missing');
+    }
+
+}))
 
 
 
